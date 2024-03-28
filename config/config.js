63b097ef20108c57ee25f92ca9083b63fcc6b9dc
Muhammed Irshad Ali K K <75845752+M0I0A@users.noreply.github.com>
2024-03-28 20:25:16 +0530

    Update config.js

--- config/config.js
+++ config/config.js
@@ -2,7 +2,10 @@ require('dotenv').config();
 
 module.exports = {
   development: {
-    use_env_variable: 'DATABASE_URL',
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
     dialect: 'postgres',
   },
   // Add other environments as needed
